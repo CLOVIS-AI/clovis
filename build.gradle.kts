4762098bbcbf33d7315c38b5940d8c35698c151b
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-02-09 19:09:20 +0100

    upgrade: Kotlin 1.4.21 → 1.4.30, moved plugin declaration to project root

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,3 +5,10 @@ subprojects {
 	group = rootProject.group
 	version = rootProject.version
 }
+
+plugins {
+	// These plugins must be declared at the top-level, even if they are not used here.
+	kotlin("multiplatform") version "1.4.30" apply false
+	kotlin("jvm") version "1.4.30" apply false
+	kotlin("plugin.serialization") version "1.4.30" apply false
+}
