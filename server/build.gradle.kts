b1d09c707ca7b8beff33667798df2e87cded8fd3
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-02-09 17:44:06 +0100

    build(gradle): Created subproject ‘server’

---
+++ server/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+	kotlin("jvm") version "1.4.21"
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation(kotlin("stdlib"))
+
+	implementation(project(":core"))
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
 rootProject.name = "clovis"
 
 include("core")
+include("server")
