0d68ed4734f1bd50c61c79c133fe892af4c51a24
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-02-09 17:13:24 +0100

    build(gradle): Created subproject ‘core‘

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,7 @@
 group = "braindot"
 version = "1.0-SNAPSHOT"
+
+subprojects {
+	group = rootProject.group
+	version = rootProject.version
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,3 @@
 rootProject.name = "clovis"
+
+include("core")
