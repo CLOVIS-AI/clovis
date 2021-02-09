ca5aa63d3d1baef712d543551f7cbaeb12fedf5f
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-02-09 17:51:59 +0100

    build(server): Start the server from Gradle

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
 	kotlin("jvm") version "1.4.21"
+	id("application")
 }
 
 repositories {
@@ -10,4 +11,8 @@ dependencies {
 	implementation(kotlin("stdlib"))
 
 	implementation(project(":core"))
+}
+
+application {
+	mainClass.set("clovis.server.ServerKt")
 }
