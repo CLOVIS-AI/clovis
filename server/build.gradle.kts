45723846cfd2a2cc8d84dfe780d642ee7ba4ec37
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-02-09 18:19:24 +0100

    upgrade(server): Added Ktor, Serialization and Logback

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -10,6 +10,16 @@ repositories {
 dependencies {
 	implementation(kotlin("stdlib"))
 
+	fun ktor(name: String) = "io.ktor:ktor-$name:1.5.1"
+	implementation(ktor("server-core"))
+	implementation(ktor("server-netty"))
+	implementation(ktor("serialization"))
+
+	fun kotlinx(name: String, version: String) = "org.jetbrains.kotlinx:kotlinx-$name:$version"
+	implementation(kotlinx("serialization-core", "1.1.0-RC"))
+
+	implementation("ch.qos.logback:logback-classic:1.2.3")
+
 	implementation(project(":core"))
 }
 
