22062dc51c937dadbfeca1ea4772d3eec7c42f88
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-02-22 18:03:04 +0100

    feat(server): Query the user data from /ping from the database

--- server/src/main/kotlin/Ping.kt
+++ server/src/main/kotlin/Ping.kt
@@ -2,26 +2,33 @@ package clovis.server
 
 import clovis.core.profile.Profile
 import clovis.core.users.User
+import clovis.server.db.Profiles
+import clovis.server.db.Users
+import clovis.server.db.withDatabase
 import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import org.jetbrains.exposed.sql.select
 
 fun Route.pingRouting() {
 	route("/ping") {
 		get("{id}") {
-			val id = call.parameters["id"]?.toIntOrNull() ?: return@get call.respondText(
+			val id = call.parameters["id"]?.toLongOrNull() ?: return@get call.respondText(
 				"Malformed id, expected an integer",
 				status = HttpStatusCode.BadRequest
 			)
 
-			if (id != 1)
-				return@get call.respondText(
-					"The ID should be 1, found $id",
-					status = HttpStatusCode.BadRequest
-				)
+			val result = withDatabase {
+				val query = (Users innerJoin Profiles)
+					.select { Users.id eq id }
+					.single()
 
-			call.respond(User(1, Profile("Mel Caller", "mel.caller@email.com")))
+				val profile = Profile(query[Profiles.fullName], query[Profiles.email])
+				User(query[Users.id].value, profile)
+			}
+
+			call.respond(result)
 		}
 	}
 }
