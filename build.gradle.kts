73c9be179ed0968735160959e0d8e18a685648ab
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-03-10 08:59:42 +0100

    upgrade: Added dependencies on Compose

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,3 +12,27 @@ plugins {
 	kotlin("jvm") version Version.kotlin apply false
 	kotlin("plugin.serialization") version Version.kotlin apply false
 }
+
+buildscript {
+	repositories {
+		google()
+		mavenCentral()
+		jcenter()
+		maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+	}
+
+	dependencies {
+		classpath("org.jetbrains.compose:compose-gradle-plugin:0.3.2")
+		classpath("com.android.tools.build:gradle:4.1.2")
+		classpath(kotlin("gradle-plugin", version = Version.kotlin))
+	}
+}
+
+allprojects {
+	repositories {
+		google()
+		mavenCentral()
+		jcenter()
+		maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+	}
+}
