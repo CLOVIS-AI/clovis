617eb3967bee10b5469cb33234f8763db6c1c06e
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-04-03 13:45:05 +0200

    feat(client): Added `Client.get` (instead of `Client.client.get`)

--- client/src/commonMain/kotlin/clovis.client/Client.kt
+++ client/src/commonMain/kotlin/clovis.client/Client.kt
@@ -19,4 +19,30 @@ class Client(val url: Url) : Closeable {
 	 * @see HttpClient.close
 	 */
 	override fun close() = client.close()
+
+	//region Requests
+
+	internal suspend inline fun <reified T> request(
+		method: HttpMethod,
+		urlString: String,
+		block: RequestBuilderDSL = {}
+	): RequestResult<T> {
+		println("Requesting $method $urlString")
+		return try {
+			val response: T = client.request(urlString) {
+				this.method = method
+				block()
+			}
+			Success(response)
+		} catch (e: ClientRequestException) {
+			HttpFailure(e.response.status, e.response.readText(), e)
+		}
+	}
+
+	internal suspend inline fun <reified T> get(
+		urlString: String,
+		block: RequestBuilderDSL = {}
+	) = request<T>(HttpMethod.Get, urlString, block)
+
+	//endregion
 }
