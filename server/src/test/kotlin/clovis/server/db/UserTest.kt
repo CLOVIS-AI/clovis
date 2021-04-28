db337d97739eeac9794dbc3a6bc47f26d4f12785
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-04-28 08:54:59 +0200

    tests(server): Test the failure of the insertion of two duplicate users

--- server/src/test/kotlin/clovis/server/db/UserTest.kt
+++ server/src/test/kotlin/clovis/server/db/UserTest.kt
@@ -25,4 +25,22 @@ class UserTest {
 		assert(user.isRight()) { user }
 	}
 
+	@Test
+	fun `duplicate users`() = runBlocking {
+		val email = "duplicate.fr"
+		val fullName = "my full name"
+
+		suspend fun createUser(): Either<DatabaseException, Unit> = either {
+			val id = Users.create(email, "some random hashed password", fullName).bind()
+			val user = Users.withId(id).bind()
+
+			assertEquals(User(id, Profile(fullName, email)), user)
+		}
+
+		val user1 = createUser()
+		val user2 = createUser()
+
+		assert(user1.isLeft() || user2.isLeft()) { "Was expecting one of the two users to fail (because they are duplicates of each other); found:\n$user1\n$user2" }
+	}
+
 }
