81c8019ca67234f4c4f62d89ab3a281872c39fc9
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-05-29 13:52:49 +0200

    tests(server): Added a test user to TestData

--- server/src/main/kotlin/clovis/server/db/TestData.kt
+++ server/src/main/kotlin/clovis/server/db/TestData.kt
@@ -1,23 +1,28 @@
 package clovis.server.db
 
+import arrow.core.Either
+import arrow.core.computations.either
+import clovis.server.DatabaseException
 import org.jetbrains.exposed.sql.deleteAll
-import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.insertAndGetId
 
 @Suppress("UNUSED_VARIABLE")
-suspend fun testData() = withDatabase {
+suspend fun testData(): Either<DatabaseException, Unit> = either {
+	withDatabase {
+		Users.deleteAll()
+		Profiles.deleteAll()
 
-	Users.deleteAll()
-	Profiles.deleteAll()
-
-	val melCaller = Users.insert { user ->
-		user[id] = 1L
-		user[hashedPassword] = "THIS IS A FAKE HASHED PASSWORD."
-		user[profile] = Profiles.insertAndGetId { profile ->
-			profile[id] = 1
-			profile[email] = "mel.caller@email.com"
-			profile[fullName] = "Mel Caller"
+		val melCaller = Users.insertAndGetId { user ->
+			user[id] = 1L
+			user[hashedPassword] = "THIS IS A FAKE HASHED PASSWORD."
+			user[profile] = Profiles.insertAndGetId { profile ->
+				profile[id] = 1
+				profile[email] = "mel.caller@email.com"
+				profile[fullName] = "Mel Caller"
+			}
 		}
-	}
+	}.bind()
 
+	val test = Users.create("test", "FAKE PASSWORD", "test") //TODO: real hashed password
+		.bind()
 }
