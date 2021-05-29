0ca1cce069d133368b58e839971682dea2b2b36c
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-05-29 12:09:01 +0200

    tests(client): Test unauthenticated access

--- client/src/commonMain/kotlin/clovis.client/users/User.kt
+++ client/src/commonMain/kotlin/clovis.client/users/User.kt
@@ -5,3 +5,9 @@ import clovis.core.api.User
 
 suspend fun Client.getUser(id: Long) =
 	get<User>("$url/ping/anonymous/$id")
+
+/**
+ * Gets the [User] account this [Client] is authenticated as.
+ */
+suspend fun Client.getMe() =
+	get<User>("$url/users/me")
--- client/src/commonTest/kotlin/clovis/client/users/UserTest.kt
+++ client/src/commonTest/kotlin/clovis/client/users/UserTest.kt
@@ -1,11 +1,12 @@
 package clovis.client.users
 
+import clovis.client.HttpFailure
 import clovis.client.RequestResult
 import clovis.client.Success
-import clovis.client.assertIsSuccess
 import clovis.client.runTestWithClient
 import clovis.core.api.Profile
 import clovis.core.api.User
+import io.ktor.http.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
@@ -19,6 +20,14 @@ class UserTest {
 
 		assertTrue(actual is Success)
 		assertEquals(expected, actual.result)
+	}
+
+	@Test
+	fun getMeUnauthenticated() = runTestWithClient {
+		val actual = getMe()
+
+		assertTrue(actual is HttpFailure)
+		assertEquals(HttpStatusCode.Unauthorized, actual.code)
 	}
 
 }
