6f1ada68de064c66ad9fc4dc113c6006255236ad
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-09-04 19:08:13 +0200

    upgrade(money.database): DataStax mappers

--- money.database/build.gradle.kts
+++ money.database/build.gradle.kts
@@ -1,8 +1,12 @@
 plugins {
 	kotlin("jvm")
+	kotlin("kapt")
 }
 
 dependencies {
 	api(project(":core:database"))
 	api(project(":money"))
+
+	implementation("com.datastax.oss:java-driver-mapper-runtime:_")
+	kapt("com.datastax.oss:java-driver-mapper-processor:_")
 }
