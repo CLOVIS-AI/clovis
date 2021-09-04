9528aff92742071142b66eed7881b8feb8891563
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-09-04 19:04:55 +0200

    build(core.database): The core database doesn't need the primitives, query builders nor the mapper

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -3,10 +3,6 @@ plugins {
 }
 
 dependencies {
-	api(project(":core:primitives"))
-
 	// Apache Cassandra drivers, by DataStax
 	api("com.datastax.oss:java-driver-core:_")
-	api("com.datastax.oss:java-driver-query-builder:_")
-	api("com.datastax.oss:java-driver-mapper-runtime:_")
 }
