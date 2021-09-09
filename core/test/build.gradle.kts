f92784b5c93a932008b5056e2ee7bec47f992bd4
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-09-09 23:38:26 +0200

    build(core.test): The test library now depends on Kotlin.test

--- core/test/build.gradle.kts
+++ core/test/build.gradle.kts
@@ -14,6 +14,21 @@ kotlin {
 		val commonMain by getting {
 			dependencies {
 				api(project(":core:primitives"))
+
+				api(kotlin("test-common"))
+				api(kotlin("test-annotations-common"))
+			}
+		}
+
+		val jvmMain by getting {
+			dependencies {
+				api(kotlin("test-junit"))
+			}
+		}
+
+		val jsMain by getting {
+			dependencies {
+				api(kotlin("test-js"))
 			}
 		}
 	}
