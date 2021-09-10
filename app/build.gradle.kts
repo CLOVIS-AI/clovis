858dd90a8f4a33ff1f256d7b568a66f239b2630b
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-09-10 22:50:50 +0200

    upgrade(app): JetBrains Compose (Desktop + Web)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,42 @@ plugins {
 	id("org.jetbrains.compose")
 }
 
+repositories {
+	mavenCentral()
+	maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+	google()
+}
+
 kotlin {
 	jvm()
 	js {
 		browser()
+		binaries.executable()
 	}
 
 	sourceSets {
 		val commonMain by getting {
 			dependencies {
 				api(project(":core"))
+
+				api(compose.runtime)
+			}
+		}
+
+		val jvmMain by getting {
+			dependencies {
+				api(compose.desktop.currentOs)
+
+				api(compose.foundation)
+				api(compose.material)
+
+				implementation(compose.preview)
+			}
+		}
+
+		val jsMain by getting {
+			dependencies {
+				api(compose.web.core)
 			}
 		}
 	}
