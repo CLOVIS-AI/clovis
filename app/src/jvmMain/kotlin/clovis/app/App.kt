34529f71ca4f3b076eb9f72412d609ff79aec607
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-09-10 23:22:41 +0200

    feat(app): JVM GUI Hello World

--- app/src/jvmMain/kotlin/clovis/app/App.kt
+++ app/src/jvmMain/kotlin/clovis/app/App.kt
@@ -1,5 +1,20 @@
 package clovis.app
 
-fun main() {
-	println("Hello world!")
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+
+fun main() = application {
+	Window(
+		onCloseRequest = ::exitApplication,
+		title = "CLOVIS Assistant",
+		state = rememberWindowState(width = 300.dp, height = 300.dp)
+	) {
+		MaterialTheme {
+			Text("Hello world!")
+		}
+	}
 }
