f14020a7057eb3bbcfe26c8a2ffbf4c0548726f3
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-09-18 12:42:51 +0200

    feat(core.database): Escape identifiers and string literals

---
+++ core.database/src/main/kotlin/clovis/database/utils/Escapes.kt
@@ -0,0 +1,17 @@
+package clovis.database.utils
+
+import com.datastax.oss.driver.api.core.CqlIdentifier
+
+/**
+ * Escapes this [String] so it can be used as an identifier in CQL.
+ */
+internal fun String.escapeAsIdentifier(): String =
+	CqlIdentifier.fromCql(this).asCql(false)
+
+/**
+ * Escapes this [String] so it can be used as a string literal in CQL.
+ */
+internal fun String.asStringLiteral(): String {
+	val escaped = replace("'", "&apos")
+	return "'$escaped'"
+}
