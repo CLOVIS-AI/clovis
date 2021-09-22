45dc84d67f6fbaf8c57a5de669b99b9e7ffd4184
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2021-09-22 22:22:06 +0200

    feat(app): Created a very simple login form

--- app/src/commonMain/kotlin/clovis.app/App.kt
+++ app/src/commonMain/kotlin/clovis.app/App.kt
@@ -1,9 +1,25 @@
 package clovis.app
 
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import clovis.ui.composables.Button
+import clovis.ui.composables.PasswordInput
 import clovis.ui.composables.Text
+import clovis.ui.composables.TextInput
+import clovis.ui.layout.Column
 
 @Composable
 fun App() {
-	Text("Hello World")
+	var email by remember { mutableStateOf("") }
+	var password by remember { mutableStateOf("") }
+
+	Column {
+		TextInput(email, onChange = { email = it })
+		PasswordInput(password, onChange = { password = it })
+
+		Button(
+			onClick = {}
+		) {
+			Text("Login")
+		}
+	}
 }
