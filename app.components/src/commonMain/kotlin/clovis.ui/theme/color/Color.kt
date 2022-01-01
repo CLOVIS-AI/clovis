e04a82c95e48e279ee367b204ed7574b18377c1b
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2022-01-01 19:09:01 +0100

    feat(app.components): Added TailwindCSS colors

--- app.components/src/commonMain/kotlin/clovis.ui/theme/color/Color.kt
+++ app.components/src/commonMain/kotlin/clovis.ui/theme/color/Color.kt
@@ -4,17 +4,31 @@ package clovis.ui.theme.color
  * Color representation, with transparency.
  */
 data class Color(
+	val red: UByte,
+
+	val green: UByte,
+
+	val blue: UByte,
 
 	/**
 	 * Opacity.
 	 *
 	 * 0 is fully transparent, 255 is not transparent at all.
 	 */
-	val alpha: UByte,
+	val alpha: UByte = 255u,
+) {
 
-	val red: UByte,
+	companion object
+}
 
-	val green: UByte,
+fun Color.Companion.fromHex(hex: String): Color {
+	val withoutHash = hex.removePrefix("#")
 
-	val blue: UByte,
-)
+	require(withoutHash.length == 6) { "An hexadecimal color representation should container 6 digits, but ${withoutHash.length} digits were found: '$withoutHash'" }
+
+	val red = withoutHash.substring(0, 2).toUByte(radix = 16)
+	val green = withoutHash.substring(2, 4).toUByte(radix = 16)
+	val blue = withoutHash.substring(4, 6).toUByte(radix = 16)
+
+	return Color(red, green, blue)
+}
