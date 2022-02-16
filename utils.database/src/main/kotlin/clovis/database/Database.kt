78f2bdf66c8b15daaaf1e00427b0e55a18101fd9
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2022-02-16 22:39:16 +0100

    feat(utils.database): Override the DataStax configuration by creating /etc/application.conf

--- utils.database/src/main/kotlin/clovis/database/Database.kt
+++ utils.database/src/main/kotlin/clovis/database/Database.kt
@@ -6,8 +6,11 @@ import clovis.logger.WithLogger
 import clovis.logger.info
 import clovis.logger.trace
 import com.datastax.oss.driver.api.core.CqlSession
+import com.datastax.oss.driver.api.core.config.DriverConfigLoader
 import com.datastax.oss.driver.api.core.cql.AsyncResultSet
 import com.datastax.oss.driver.api.core.cql.Statement
+import com.datastax.oss.driver.internal.core.config.typesafe.DefaultDriverConfigLoader
+import java.io.File
 
 /**
  * Represents a connection to the database.
@@ -49,7 +52,22 @@ class Database(
 		 * see [the driver documentation](https://docs.datastax.com/en/developer/java-driver/4.13/manual/core/configuration/reference/).
 		 */
 		suspend fun connect(): Database {
+			val bonusConfiguration = File("/etc/application.conf")
+			val defaultConfiguration = DefaultDriverConfigLoader()
+
+			val config = when {
+				bonusConfiguration.isFile && bonusConfiguration.canRead() -> {
+					log.trace { "The file ${bonusConfiguration.absolutePath} overrides the default configuration." }
+					DriverConfigLoader.fromFile(bonusConfiguration) + defaultConfiguration
+				}
+				else -> {
+					log.info { "To override the default configuration, create the file ${bonusConfiguration.absolutePath}." }
+					defaultConfiguration
+				}
+			}
+
 			val session = CqlSession.builder()
+				.withConfigLoader(config)
 				.buildAsync()
 				.await()
 
@@ -58,3 +76,10 @@ class Database(
 
 	}
 }
+
+/**
+ * Combines the [DriverConfigLoader] configuration.
+ *
+ * The receiver (`this`) has priority over the [other] configuration.
+ */
+private operator fun DriverConfigLoader.plus(other: DriverConfigLoader) = DriverConfigLoader.compose(this, other)
